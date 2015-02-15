d1f3e2bd61fbf121cb51383ffab2363aaa66ef26
imrul <imrul001@gmail.com>
2015-02-15 14:11:58 +0700

    arsClient: Added a simple login page..
    Signed-off-by: imrul <imrul001@gmail.com>

---
+++ arsClient/index__.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <div style="width:50%; margin:0 auto">
+            <img src="./cover.jpg">
+        </div>
+        <div>
+            employees
+        </div>
+    </body>
+</html>
