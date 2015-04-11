9c66ef52bba9a25ae3658d206b1174efcdd337ab
imrul <imrul001@gmail.com>
2015-04-11 17:14:05 +0700

    arsClient: Fix some bugs arsClient of Mysql..
    Signed-off-by: imrul <imrul001@gmail.com>

--- arsClient/application/views/home_view.php
+++ arsClient/application/views/home_view.php
@@ -34,12 +34,12 @@
               <div id="userOptionsContainer">
                  <div id="tabs">
                    <ul>
-                     <li><a href="#tabs-1">Query</a></li>                     
+<!--                      <li><a href="#tabs-1">Query</a></li>                      -->
                      <li><a href="#tabs-7">Add Employee</a></li>
                      <li><a href="#tabs-8">Summary</a></li>
                      <li><a href="#tabs-9">Search</a></li>
                   </ul>
-                 <div id="tabs-1">
+                 <!-- <div id="tabs-1">
                      <form id="form_report">
                          <input id="salarybytitle_button" type="submit" value="Variation Salary by Title">
                          <input id="totalempbyyear_button" type="submit" value="Total Employee by Year">
@@ -50,7 +50,7 @@
                      </form>
                      
                      <div id="result_table"></div>
-                 </div>              
+                 </div> -->              
                  <div id="tabs-7">
                    <?php $this->load->view('add_employee_form');?>
                    <div id="emp_result"></div> 
@@ -97,14 +97,7 @@
                 $('#dept_no')[0].selectedIndex = 0;
                 $('#title')[0].selectedIndex = 0;
                 $("#emp_result").html("");
-                    $.ajax({
-                        type: "POST",
-                        
-                        failure: function () {
-                            alert("error");
-                        }
-                    });
-                    return false;
+                return false;
             });
             $("#addemployee_button").on("click", function () {
                     var url = "<?php echo base_url(); ?>index.php/report_control/addemployee";
@@ -114,42 +107,46 @@
                     var pastYear = past.getTime();
                     var age = Math.floor((nowYear - pastYear)/(365.25 * 24 * 60 * 60 * 1000));
                                         
-                    if($('#gender-0').val()==="M"){gender="M";}
-                    else gender="F";
+                    // if($('#gender-0').val()==="M"){gender="M";}
+                    // else gender="F";
                    
-                    if($('#emp_no').val()===""){alert("<Employee No> cannot be empty.");$( '#emp_no' ).focus();}                   
-                    else if($('#first_name').val()===""){alert("<First Name> cannot be empty."); $( '#first_name' ).focus();}
-                    else if($('#last_name').val()===""){alert("<Last Name> cannot be empty.");$( '#last_name' ).focus();}
-                    else if($('#birth_date').val()===""){alert("<Birth Date> cannot be empty.");$( '#birth_date' ).focus();}
-                    else if(age<18){alert("Employee's age must be greater than or equal to 18 years old.");$( '#birth_date' ).focus();}
-                    else if($('#dept_no').val()===""){alert("<Department> cannot be empty.");$( '#dept_no' ).focus();}
-                    else if($('#title').val()===""){alert("<Title> cannot be empty.");$( '#title' ).focus();}
-                    else if($('#from_date').val()===""){alert("<From Date> cannot be empty.");$( '#from_date' ).focus();}
-                    else if($('#to_date').val()===""){alert("<To Date> cannot be empty.");$( '#to_date' ).focus();}
-                    else if($('#from_date').val()<=$('#birth_date').val()){alert("<From Date> must be greater than <Birth Date>.");}                    
-                    else if($('#from_date').val()>=$('#to_date').val()){alert("<From Date> must be less than <To Date>.");}
-                    else if($('#salary').val()===""){alert("<Salary> cannot be empty.");$( '#salary' ).focus();}
-                    else if(!$.isNumeric($('#salary').val())){alert("<Salary> must be numeric.");$( '#salary' ).focus();}
-                    else{
-                        $.ajax({
-                            type: "POST",
-                            url: url,
-                            data: "emp_no=" +  $('#emp_no').val()+ "&first_name=" + $('#first_name').val() +
-                                  "&last_name=" + $('#last_name').val() + "&gender=" + gender +
-                                  "&birth_date=" + $('#birth_date').val() + 
-                                  "&dept_no="+ $('#dept_no').val()+ "&from_date=" + $('#from_date').val()+
-                                  "&to_date="+ $('#to_date').val()+ "&salary="+ $('#salary').val()+
-                                  "&title="+ $('#title').val(),
-
-                            success: function (data) {
-                               $("#emp_result").html(data);
-                            },
-                            failure: function () {
-                                alert("error");
-                            }
-                        });
-                    }
-                    return false;
+                    // if($('#emp_no').val()===""){alert("<Employee No> cannot be empty.");$( '#emp_no' ).focus();}                   
+                    // else if($('#first_name').val()===""){alert("<First Name> cannot be empty."); $( '#first_name' ).focus();}
+                    // else if($('#last_name').val()===""){alert("<Last Name> cannot be empty.");$( '#last_name' ).focus();}
+                    // else if($('#birth_date').val()===""){alert("<Birth Date> cannot be empty.");$( '#birth_date' ).focus();}
+                    // else if(age<18){alert("Employee's age must be greater than or equal to 18 years old.");$( '#birth_date' ).focus();}
+                    // else if($('#dept_no').val()===""){alert("<Department> cannot be empty.");$( '#dept_no' ).focus();}
+                    // else if($('#title').val()===""){alert("<Title> cannot be empty.");$( '#title' ).focus();}
+                    // else if($('#from_date').val()===""){alert("<From Date> cannot be empty.");$( '#from_date' ).focus();}
+                    // else if($('#to_date').val()===""){alert("<To Date> cannot be empty.");$( '#to_date' ).focus();}
+                    // else if($('#from_date').val()<=$('#birth_date').val()){alert("<From Date> must be greater than <Birth Date>.");}                    
+                    // else if($('#from_date').val()>=$('#to_date').val()){alert("<From Date> must be less than <To Date>.");}
+                    // else if($('#salary').val()===""){alert("<Salary> cannot be empty.");$( '#salary' ).focus();}
+                    // else if(!$.isNumeric($('#salary').val())){alert("<Salary> must be numeric.");$( '#salary' ).focus();}
+                    // else{
+                    if ($('#emp_no').val() != "" && $('#first_name').val() != "" && $('#last_name').val() != "" && $('#salary').val() != ""
+                        && $('#from_date').val() != "" && $('#to_date').val() != "" && $('#birth_date').val() != "") {
+                    $.ajax({
+                        type: "POST",
+                        url: url,
+                        data: "emp_no=" + $('#emp_no').val() + "&first_name=" + $('#first_name').val() +
+                                "&last_name=" + $('#last_name').val() + "&gender=" + gender +
+                                "&birth_date=" + $('#birth_date').val() +
+                                "&dept_no=" + $('#dept_no').val() + "&from_date=" + $('#from_date').val() +
+                                "&to_date=" + $('#to_date').val() + "&salary=" + $('#salary').val() +
+                                "&title=" + $('#title').val(),
+                        success: function (data) {
+                            $("#emp_result").html(data);
+                        },
+                        failure: function () {
+                            alert("error");
+                        }
+                    });
+                } else {
+                    alert("All Fields are required");
+                }
+//                    }
+                return false;
             });   
             $("#salarybytitle_button").on("click", function () {
                     var url = "<?php echo base_url(); ?>index.php/report_control/salarybytitle";
