e3f77c41a7103459dbb45f04430dbd757ed9ec4d
imrul <imrul001@gmail.com>
2015-04-24 16:31:58 +0700

    arsClient_m: add addEmployee query for mongoDB              add some search queries..
    Signed-off-by: imrul <imrul001@gmail.com>

--- arsClient_m/application/models/model_employees.php
+++ arsClient_m/application/models/model_employees.php
@@ -7,44 +7,38 @@
  * */
 class Model_employees extends CI_Model {
 
-    function addemployee($emp_no, $first_name, $last_name, $gender, $birth_date, $dept_no, $from_date, $to_date, $salary, $title) {
+    function addemployee($emp_no, $first_name, $last_name, $gender, $birth_date, $dept_name, $from_date, $to_date, $salary, $title) {
+        
         $m = new MongoClient();
-        $db = $m->TestMongoDB;
-        $c1 = $db->employees;
-        $c2 = $db->dept_emp;
-        $c3 = $db->titles;
-        $c4 = $db->salaries;
+        $db = $m->employees;
+        $collection = $db->empCollection;
         $arrayName = array('emp_no' => (int) $emp_no);
         $response = "0";
-        $check = $c1->count($arrayName);
-
+        $check = $collection->count($arrayName);
         if ($check == 0) {
+
+            $deptArray[0] = array("from_date" => new MongoDate(strtotime($from_date)),
+                "to_date" => new MongoDate(strtotime($to_date)),
+                "dept_name" => $dept_name);
+            $dept_manager = "";
+            $salaryArray[0] = array("from_date" => new MongoDate(strtotime($from_date)),
+                "to_date" => new MongoDate(strtotime($to_date)),
+                "salary" => (int) $salary);
+            $titleArray[0] = array("from_date" => new MongoDate(strtotime($from_date)),
+                "to_date" => new MongoDate(strtotime($to_date)),
+                "title" => $title);
             $empArray = array('emp_no' => (int) $emp_no,
                 'first_name' => $first_name,
                 'last_name' => $last_name,
                 'gender' => $gender,
-                'birth_date' => date('Y-m-d', strtotime($birth_date)),
-                'hire_date' => date('Y-m-d', strtotime($from_date)));
-
-            $deptArray = array('emp_no' => (int)$emp_no,
-                'dept_no' => $dept_no,
-                'from_date' => date('Y-m-d', strtotime($from_date)),
-                'to_date' => date('Y-m-d', strtotime($to_date)));
-
-            $titlesArray = array('emp_no' => (int)$emp_no,
-                'title' => $title,
-                'from_date' => date('Y-m-d', strtotime($from_date)),
-                'to_date' => date('Y-m-d', strtotime($to_date)));
-
-            $salaryArray = array('emp_no' => (int)$emp_no,
-                'salary' => (int) $salary,
-                'from_date' => date('Y-m-d', strtotime($from_date)),
-                'to_date' => date('Y-m-d', strtotime($to_date)));
-
-            $c1->insert($empArray);
-            $c2->insert($deptArray);
-            $c3->insert($titlesArray);
-            $c4->insert($salaryArray);
+                'birth_date' => new MongoDate(strtotime($birth_date)),
+                'hire_date' => new MongoDate(strtotime($from_date)),
+                'dept_emp' => $deptArray,
+                'dept_manager' => $dept_manager,
+                'salaries' => $salaryArray,
+                'titles' => $titleArray
+            );
+            $collection->insert($empArray);
             $response = "1";
         }
         return $response;
@@ -56,18 +50,6 @@ class Model_employees extends CI_Model {
         $collection = $db->empCollection;
         $empList = $collection->find()->limit(2);
         return $empList;
-    }
-
-    function salarybytitle() {
-
-        $sql = " SELECT titles.title, salaries.salary As salary " .
-                " FROM salaries, titles" .
-                " WHERE salaries.emp_no=titles.emp_no" .
-                " LIMIT 30";
-
-
-        $q1 = $this->db->query($sql);
-        return $q1->result();
     }
 
     function getDepartments() {
@@ -88,108 +70,63 @@ class Model_employees extends CI_Model {
         return $postList;
     }
 
-    function totalempbydept() {
-
-        $sql = " SELECT  DISTINCT departments.dept_name, COUNT(departments.dept_name) as total" .
-                " FROM `dept_emp`, `departments`" .
-                " WHERE departments.dept_no=dept_emp.dept_no" .
-                " GROUP BY departments.dept_name";
-
-        $q1 = $this->db->query($sql);
-        return $q1->result();
-    }
-
-    function totalsalaryexpensebydept() {
-
-        $sql = " SELECT  DISTINCT departments.dept_name, SUM(salaries.salary) as total" .
-                " FROM departments,dept_emp, salaries" .
-                " WHERE salaries.emp_no=dept_emp.emp_no and dept_emp.dept_no=departments.dept_no" .
-                " GROUP BY departments.dept_name";
-
-        $q1 = $this->db->query($sql);
-        return $q1->result();
-    }
-
-    function listmanagerbydept() {
-
-        $sql = " SELECT departments.dept_name,employees.first_name,employees.last_name, dept_manager.from_date, dept_manager.to_date" .
-                " FROM dept_manager, employees, departments" .
-                " WHERE departments.dept_no=dept_manager.dept_no AND dept_manager.emp_no=employees.emp_no" .
-                " ORDER BY departments.dept_name, dept_manager.from_date";
-
-        $q1 = $this->db->query($sql);
-        return $q1->result();
-    }
-
-    function totalempbyyear() {
-
-        $sql = " SELECT YEAR(hire_date) AS year,COUNT(emp_no) AS total" .
-                " FROM employees" .
-                " GROUP BY YEAR(hire_date)";
-
-        $q1 = $this->db->query($sql);
-        return $q1->result();
-    }
-
-    function totalempbyyeardept() {
-
-        $sql = " SELECT YEAR(hire_date) AS year, Departments.dept_name, COUNT(employees.emp_no) AS total" .
-                " FROM employees, departments, dept_emp " .
-                " WHERE employees.emp_no=dept_emp.emp_no AND dept_emp.dept_no=departments.dept_no" .
-                " Group BY YEAR(employees.hire_date), departments.dept_name";
-
-        $q1 = $this->db->query($sql);
-        return $q1->result();
-    }
-
 //    Search Query
 
     function getEmployeeById($id, $limit) {
         $m = new MongoClient();
         $db = $m->employees;
         $collection = $db->empCollection;
-        $param= array('emp_no' => (int)$id);
+        $param = array('emp_no' => (int) $id);
         $empInfo = $collection->find($param)->limit($limit);
-        return $empInfo ;
+        return $empInfo;
     }
-    
+
     function getEmployeesByGender($gender, $limit) {
         $m = new MongoClient();
         $db = $m->employees;
         $collection = $db->empCollection;
-        $param= array('gender' => $gender);
+        $param = array('gender' => $gender);
         $empInfo = $collection->find($param)->limit($limit);
-        return $empInfo ;
+        return $empInfo;
     }
-    
+
     function getEmployeeByTitle($title, $limit) {
         $m = new MongoClient();
         $db = $m->employees;
         $collection = $db->empCollection;
-        $param= array('titles.title' => $title);
+        $param = array('titles.title' => $title);
         $empInfo = $collection->find($param)->limit($limit);
-        return $empInfo ;
+        return $empInfo;
     }
-    
+
     function getEmployeesByDepartment($dept, $limit) {
         $m = new MongoClient();
         $db = $m->employees;
         $collection = $db->empCollection;
-        $param= array('dept_emp.dept_name' => $dept);
+        $param = array('dept_emp.dept_name' => $dept);
         $empInfo = $collection->find($param)->limit($limit);
-        return $empInfo ;
+        return $empInfo;
     }
-    
 
     function getEmployeeByFn($pattern, $limit) {
-        $sql = " SELECT employees.emp_no, employees.first_name, employees.last_name, employees.gender, departments.dept_name,titles.title" .
-                " FROM employees,dept_emp,departments,titles" .
-                " WHERE employees.first_name LIKE '%$pattern%' AND employees.emp_no=titles.emp_no AND departments.dept_no=dept_emp.dept_no AND titles.emp_no=dept_emp.emp_no" .
-                " LIMIT " . $limit;
-        $q1 = $this->db->query($sql);
-        return $q1->result();
+        $m = new MongoClient();
+        $db = $m->employees;
+        $collection = $db->empCollection;
+        $regex = new MongoRegex("/^" . $pattern . "/i");
+        $param = array('first_name' => $regex);
+        $empInfo = $collection->find($param)->limit($limit);
+        return $empInfo;
     }
 
+    function getEmployeeByLn($pattern, $limit) {
+        $m = new MongoClient();
+        $db = $m->employees;
+        $collection = $db->empCollection;
+        $regex = new MongoRegex("/^" . $pattern . "/i");
+        $param = array('last_name' => $regex);
+        $empInfo = $collection->find($param)->limit($limit);
+        return $empInfo;
+    }
 }
 ?>
 
--- arsClient_m/application/views/search_view.php
+++ arsClient_m/application/views/search_view.php
@@ -229,7 +229,7 @@
                 }
             }
 
-        })
+        });
     }
 
     function postQueryParam(methodName) {
